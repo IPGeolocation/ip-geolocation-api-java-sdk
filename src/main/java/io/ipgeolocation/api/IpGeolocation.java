657634230b417f166371b27ce93f0878e09845e7
Usman <usman@jfreaks.com>
2018-07-30 21:31:40 +0500

    Query builder method updated

--- src/main/java/io/ipgeolocation/api/IpGeolocation.java
+++ src/main/java/io/ipgeolocation/api/IpGeolocation.java
@@ -7,6 +7,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
 import com.google.gson.Gson;
 
 public class IpGeolocation {
@@ -40,30 +42,18 @@ public class IpGeolocation {
         return getBadRequestResponse();
     }
 
-    private String buildQuery(Map<String, String> parameters) {
+    private String buildQuery(Map<String, String> params) {
         String query = "";
-        String ip = null;
-        String fields = null;
-        String tz = null;
-        if(parameters != null && parameters.size() > 0) {
-            ip = parameters.get("ip") != null ? parameters.get("ip") : "";
-            fields = parameters.get("fields") != null ? parameters.get("fields") : "";
-            tz = parameters.get("tz") != null ? parameters.get("tz") : "";
-        }
 
         if(this.apiKey != null && this.apiKey.trim().length() > 0) {
             query = "apiKey=" + this.apiKey;
+        }
 
-            if(ip != null && ip.trim().length() > 0) {
-                query = query + ("&ip=" + ip);
-            }
+        if(params != null && params.size() > 0) {
+            Set<String> paramNames = params.keySet();
 
-            if(fields != null && fields.trim().length() > 0) {
-                query = query + ("&fields=" + fields);
-            }
-
-            if(tz != null && tz.trim().length() > 0) {
-                query = query + ("&tz=" + tz);
+            for(String paramName: paramNames) {
+                query = query.concat("&" + paramName + "=" + params.get(paramName));
             }
         }
         return query;
@@ -101,7 +91,7 @@ public class IpGeolocation {
         return getResponse(responseCode, response.toString());
     }
 
-    private Map<String, String> getResponse(int responseCode, String response){
+    private Map<String, String> getResponse(int responseCode, String response) {
         Gson gson = new Gson();
         Map<String, String> map = new HashMap<String, String>();
         map = (Map<String, String>) gson.fromJson(response, map.getClass());
@@ -112,7 +102,7 @@ public class IpGeolocation {
     private Map<String, String> getBadRequestResponse() {
         Map<String, String> response = new HashMap<String, String>();
         response.put("status", "404");
-        response.put("message", "Incorrect paramater");
+        response.put("message", "Incorrect parameters");
         return response;
     }
 }
--- src/main/java/io/ipgeolocation/api/Main.java
+++ src/main/java/io/ipgeolocation/api/Main.java
@@ -8,8 +8,9 @@ public class Main {
     public static void main(String[] args) {
         IpGeolocation ipGeolocation = new IpGeolocation("ea91e4a4159247fdb0926feae70c2911");
         Map<String, String> parameters = new HashMap<String, String>();
-        parameters.put("tz", "Asia/Karachi");
-        Map<String, String> geolocation = ipGeolocation.getTimezone(parameters);
+        parameters.put("ip", "5.4.3.2");
+        parameters.put("fields", "geo,currency");
+        Map<String, String> geolocation = ipGeolocation.getIpgeo(parameters);
         System.out.println(geolocation.toString());
     }
 }
