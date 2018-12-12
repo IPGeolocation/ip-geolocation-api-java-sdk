4024d94e2ab61b33055ac13f90c1327c4d371970
Usman <usman@jfreaks.com>
2018-12-12 10:30:54 +0500

    Add currency symbol

--- src/main/java/io/ipgeolocation/api/GeolocationCurrency.java
+++ src/main/java/io/ipgeolocation/api/GeolocationCurrency.java
@@ -5,10 +5,12 @@ import java.util.Map;
 public class GeolocationCurrency {
     private String name;
     private String code;
+    private String symbol;
 
     GeolocationCurrency(Map json) {
         this.name = (String) json.get("name");
         this.code = (String) json.get("code");
+        this.symbol = (String) json.get("symbol");
     }
 
     public String getName() {
@@ -19,8 +21,12 @@ public class GeolocationCurrency {
         return code;
     }
 
+    public String getSymbol() {
+        return symbol;
+    }
+
     @Override
     public String toString() {
-        return String.format("name: '%s' \ncode: '%s'", name, code);
+        return String.format("name: '%s' \ncode: '%s' \nsymbol: '%s'", name, code, symbol);
     }
 }
